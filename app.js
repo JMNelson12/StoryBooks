56d5e0c8f82d5932a4afd637c09af3a41cda3741
Jacob Nelson <jacob_m_nelson@live.com>
2017-10-10 10:53:10 -0500

    handled auth

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const mongoose = require('mongoose');
+const passport = require('passport');
 const express = require('express'),
       app = express(),
       port = process.env.PORT || 8000;
 
+/* ---------- Passport Config ---------- */
+require('./config/passport')(passport);
+
+/* ---------- Load Routes ---------- */
+const auth = require('./routes/auth');
+
+/* ---------- Homepage Route ---------- */
 app.get('/', (req, res) => {
   res.send('working');
 });
+
+/* ---------- Use Auth Route ---------- */
+app.use('/auth', auth);
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://Jacob:story-dev@ds115045.mlab.com:15045/storybooks-dev',
+  googleClientID:'371376160431-vjoa08aoia0m10t937bahu0vklnmpi5d.apps.googleusercontent.com',
+  googleClientSecret:'dPNQiFKhqC_Bn6xxi81fMS6a'
+}
