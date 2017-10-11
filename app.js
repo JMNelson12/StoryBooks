658536900d1bc68727d83bb4b535237c229dd669
Jacob Nelson <jacob_m_nelson@live.com>
2017-10-10 21:00:57 -0500

    More Changes

--- app.js
+++ app.js
@@ -3,28 +3,47 @@ const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const path = require('path');
+const bodyParser = require('body-parser');
 const express = require('express'),
       app = express(),
       port = process.env.PORT || 8000;
 
-// Handlebars Middleware
-app.engine('handlebars', exphbs({ defaultLayout:'master' }));
-app.set('view engine', 'handlebars');
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-// Load User Model
+// Load Models
 require('./models/User');
+require('./models/Story');
 
 /* ---------- Passport Config ---------- */
 require('./config/passport')(passport);
 
 /* ---------- Load Routes ---------- */
 const auth = require('./routes/auth');
-
 /* ---------- Load Index ---------- */
 const index = require('./routes/index');
+/* ---------- Load Stories ---------- */
+const stories = require('./routes/stories');
 
 /* ---------- Load Keys File ---------- */
 const keys = require('./config/keys');
+
+// Handlebars Helpers
+const {
+  truncate,
+  stripTags,
+  formatDate
+} = require('./helpers/hbs');
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({ helpers:{
+  truncate: truncate,
+  stripTags: stripTags,
+  formatDate: formatDate
+}, defaultLayout:'master' }));
+app.set('view engine', 'handlebars');
 
 /* ---------- Mapp global promise ---------- */
 mongoose.Promise = global.Promise;
@@ -53,9 +72,13 @@ app.use((req, res, next) => {
   next();
 });
 
+// Set static folder !for public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 /* ---------- Use Routes ---------- */
 app.use('/', index);
 app.use('/auth', auth);
+app.use('/stories', stories);
 
 
 app.listen(port, () => {
